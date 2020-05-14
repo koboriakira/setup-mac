8b01c05d9582533fe3c3cb0774f1f1acda8a0584
Akira Kobori <private.beats@gmail.com>
2020-05-15 08:54:49 +0900

    Fix a typo

--- setup/configure_vscode.sh
+++ setup/configure_vscode.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 # VSCodeの設定を同期
 echo 'configure_vscode: start!' \
-&& ln -nfs ~/git/setting/VSCode/User ~/Library/Application\ Support/Code/User \
+&& ln -nfs ~/git/setting/VSCode/User/settings.json ~/Library/Application\ Support/Code/User/settings.json \
+&& ln -nfs ~/git/setting/VSCode/User/markdown-style.css ~/Library/Application\ Support/Code/User/markdown-style.css \
+&& ln -nfs ~/git/setting/VSCode/User/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json \
 && echo 'configure_vscode: success!'
