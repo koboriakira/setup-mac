29ef83fcd835ef0c562d663b804af75a8ba44ad5
Akira Kobori <private.beats@gmail.com>
2020-05-15 08:55:14 +0900

    日記のcloneを追加

--- setup/clone_setting_repository.sh
+++ setup/clone_setting_repository.sh
@@ -2,4 +2,5 @@
 echo "clone_setting_repository: start" \
 && mkdir ~/git \
 && git clone git@github.com:koboriakira/setting.git ~/git/setting \
+&& git clone git@github.com:koboriakira/Diary2020.git ~/git/diary \
 && echo "clone_setting_repository: success"
