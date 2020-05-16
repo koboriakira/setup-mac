0d992ae6f6eaea30db8665c90e7bae573e68fd23
Akira Kobori <private.beats@gmail.com>
2020-05-16 11:26:58 +0900

    Add ip command

--- setup/brew_install.sh
+++ setup/brew_install.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 echo 'brew_install: success!' \
 && brew install tree \
+&& brew install iproute2mac \
 && echo 'brew_install: success!' \
 && brew install gnu-sed \
 && brew install git
