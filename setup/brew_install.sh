82d7933dc8da37d9bdc4b99e86b9b1a635b5749e
Akira Kobori <private.beats@gmail.com>
2020-07-31 20:07:43 +0900

    lua、vimをインストール

--- setup/brew_install.sh
+++ setup/brew_install.sh
@@ -7,4 +7,8 @@ echo 'brew_install: success!' \
 && brew install coreutils \
 && brew install pstree \
 && brew install gawk \
+# vimの補完プラグインneocompleteを使うために必要な言語luaをインストール
+&& brew install lua \
+# vimの再インストール
+&& brew install vim --with-lua --with-python3 \
 && echo 'brew_install: success!'
