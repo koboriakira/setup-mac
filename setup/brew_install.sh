ba2d8b7763d9ee304e37033d03125c44012f43f0
Akira Kobori <private.beats@gmail.com>
2020-07-31 20:10:41 +0900

    luaのインストールを取り消し

--- setup/brew_install.sh
+++ setup/brew_install.sh
@@ -7,8 +7,4 @@ echo 'brew_install: success!' \
 && brew install coreutils \
 && brew install pstree \
 && brew install gawk \
-# vimの補完プラグインneocompleteを使うために必要な言語luaをインストール
-&& brew install lua \
-# vimの再インストール
-&& brew install vim --with-lua --with-python3 \
 && echo 'brew_install: success!'
