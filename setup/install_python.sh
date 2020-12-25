f0f989f5ec94589b81a439fcf46bfcbc53abca8d
Akira Kobori <private.beats@gmail.com>
2020-12-25 14:13:58 +0900

    Pythonのインストールするバージョンを更新

--- setup/install_python.sh
+++ setup/install_python.sh
@@ -1,12 +1,16 @@
 echo "install_python: start" \
+# pyenvのインストール、設定
 && brew install pyenv \
 && echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshenv \
 && echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc \
 && source ~/.zshenv \
 && source ~/.zshrc \
 && eval "$(pyenv init -)" \
-&& pyenv install 3.8.2 \
+&& pyenv install 3.9.0 \
 && pyenv rehash \
-&& pyenv global 3.8.2 \
+&& pyenv global 3.9.0 \
+# pipenvのインストール
+&& brew install pipenv \
+# よく使うライブラリのインストール
 && pip install kaggle --upgrade \
 && echo "install_python: success"
