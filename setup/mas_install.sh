4d834cd2404e5cd77ff83d7161b15cde3a51b7d5
Akira Kobori <private.beats@gmail.com>
2020-12-25 14:52:53 +0900

    Add libraries of mas

---
+++ setup/mas_install.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+echo 'mas_install: start!' \
+# LadioCast (000013000)
+&& mas install 411213048 \
+# Adobe Lightroom (3.3)
+&& mas install 1451544217 \
+# Final Cut Pro (10.4.8)
+&& mas install 424389933 \
+&& echo 'brew_install: success!'
