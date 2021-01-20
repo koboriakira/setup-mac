303a1dd70e61b3b7f6f1a236dfb0133acdaa687f
Akira Kobori <private.beats@gmail.com>
2021-01-20 09:26:04 +0900

    Update

--- setup/brewcask_install.sh
+++ setup/brewcask_install.sh
@@ -1,41 +1,42 @@
 #!/bin/zsh
-echo 'brewcask_install: start!' \
-&& brew cask install google-chrome \
-&& brew cask install spotify \
-&& brew cask install 1password \
-&& brew cask install 1password-cli \
-&& brew cask install slack \
-&& brew cask install kindle \
-&& brew cask install visual-studio-code \
-&& brew cask install ableton-live-suite \
-&& brew cask install splice \
-&& brew cask install evernote \
-&& brew cask install android-file-transfer \
-&& brew cask install mediahuman-audio-converter \
-&& brew cask install sketch \
-&& brew cask install google-japanese-ime \
-&& brew cask install deepl \
-&& brew cask install notion \
-&& brew cask install docker \
-&& brew cask install table-tool \
-&& brew cask install keycastr \
-&& brew cask install imageoptim \
-&& brew cask install rectangle \
-&& brew cask install ladiocast \
-&& brew cask install obs \
-&& brew cask install parallels \
-&& brew cask install pokerstars \
-&& brew cask install font-hack-nerd-font \
-&& brew cask install github \
-&& brew cask install todoist \
-&& brew cask install google-cloud-sdk \
-&& brew cask install postman \
-&& brew cask install licecap \
-&& brew cask install alfred \
-&& brew cask install clipy \
-# && brew cask install virtualbox \
-# && brew cask install vagrant \
-&& echo 'brewcask_install: success!'
+echo 'brew install --cask: start!' \
+&& brew install --cask google-chrome \
+&& brew install --cask spotify \
+&& brew install --cask 1password \
+&& brew install --cask 1password-cli \
+&& brew install --cask slack \
+&& brew install --cask kindle \
+&& brew install --cask visual-studio-code \
+&& brew install --cask ableton-live-suite \
+&& brew install --cask splice \
+&& brew install --cask evernote \
+&& brew install --cask android-file-transfer \
+&& brew install --cask mediahuman-audio-converter \
+&& brew install --cask sketch \
+&& brew install --cask google-japanese-ime \
+&& brew install --cask deepl \
+&& brew install --cask notion \
+&& brew install --cask docker \
+&& brew install --cask table-tool \
+&& brew install --cask keycastr \
+&& brew install --cask imageoptim \
+&& brew install --cask rectangle \
+&& brew install --cask ladiocast \
+&& brew install --cask obs \
+&& brew install --cask parallels \
+&& brew install --cask pokerstars \
+&& brew install --cask font-hack-nerd-font \
+&& brew install --cask github \
+&& brew install --cask todoist \
+&& brew install --cask google-cloud-sdk \
+&& brew install --cask postman \
+&& brew install --cask licecap \
+&& brew install --cask alfred \
+&& brew install --cask clipy \
+&& brew install --cask vlc \
+# && brew install --cask virtualbox \
+# && brew install --cask vagrant \
+&& echo 'brew install --cask: success!'
 # 手動で入れるアプリケーション
 # recordbox
 # soundflower
