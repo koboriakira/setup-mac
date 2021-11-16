096161e590db5f5b8da20df731ed6a0d30fffd3e
Kobori Akira <private.beats@gmail.com>
2021-11-16 19:46:22 +0900

    先にmasをインストールする

--- setup/mas_install.sh
+++ setup/mas_install.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 echo 'mas_install: start!'
+brew install mas
 mas install 411213048 # LadioCast (000013000)
 # mas install 1451544217 # Adobe Lightroom (3.3)
 mas install 424389933 # Final Cut Pro (10.4.8)
